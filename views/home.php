0ea72b9c3981f53d441289346b52d3ffef9e6db5
Tincho228 <martin_quintero521@hotmail.com>
2021-10-15 11:37:50 -0300

    Adding services

--- views/home.php
+++ views/home.php
@@ -6,7 +6,8 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="./css/styles.css"></link>
+  <link rel="stylesheet" href="./css/styles.css">
+  </link>
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -17,7 +18,7 @@
 </head>
 
 <body>
-  <div class="jumbotron orange text-light text-center " style="padding:100px 25px">
+  <div class="jumbotron bg-orange text-light text-center " style="padding:100px 25px">
     <h1>Royalq - Mundial</h1>
     <p>We specialize in blablabla</p>
     <form class="form-inline justify-content-center">
@@ -29,7 +30,9 @@
       </div>
     </form>
   </div>
-  <div class="container-fluid" style="padding:60px 50px">
+
+  <!-- <div class="container-fluid justify-content-center" style="padding:60px 50px">
+  <div class="col-sm-12 col-md-8">
     <div class="row">
       <div class="col-sm-8">
         <h2>About Company Page</h2>
@@ -37,16 +40,17 @@
         <p>Lorem ipsum..</p>
         <button class="btn btn-default btn-lg">Get in Touch</button>
       </div>
-      <div class="col-sm-4">
-        <span class="glyphicon glyphicon-signal logo"></span>
+      <div class="col-sm-4 text-center">
+        <i class="fas fa-signal" style="font-size: 200px;"></i>
       </div>
     </div>
-  </div>
+    </div> 
+  
 
   <div class="container-fluid bg-grey" style="padding:60px 50px">
     <div class="row">
-      <div class="col-sm-4">
-        <span class="glyphicon glyphicon-globe logo"></span>
+      <div class="col-sm-4 text-center">
+        <i class="fas fa-globe-americas" style="font-size: 200px;"></i>
       </div>
       <div class="col-sm-8">
         <h2>Our Values</h2>
@@ -55,6 +59,52 @@
       </div>
     </div>
   </div>
+  </div> -->
+  <div class="row justify-content-center">
+    <div class="col-sm-12 col-lg-8">
+      <div class="row">
+        <div class="col-sm-12 col-md-8 " style="padding: 30px 30px">
+          <h2 style="margin-bottom: 60px;">About Company Page</h2>
+          <h4 style="margin-bottom: 40px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio numquam
+            pariatur tempore quasi sit
+            minima minus rerum beatae similique nostrum temporibus sequi ipsam, mollitia libero, velit facere officiis
+            facilis fuga?</h4>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus, aliquid porro corrupti ut voluptates
+            eius laudantium tempora repudiandae fuga iste deserunt, dolores nesciunt eligendi. Iusto iste cum quae ipsa
+            numquam.</p>
+          <button class="btn btn-outline-secondary btn-lg">Get in Touch</button>
+        </div>
+        <div class="col-sm-12 col-md-4 text-center" style="padding:30px 30px">
+          <i class="fas fa-signal text-orange" style="font-size: 200px;"></i>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+  <div class="row justify-content-center bg-light">
+    <div class="col-sm-12 col-lg-8">
+      <div class="row">
+        <div class="col-sm-12 col-md-4 text-center" style="padding:50px 60px">
+          <i class="fas fa-globe-americas text-orange" style="font-size: 200px;"></i>
+        </div>
+        <div class="col-sm-12 col-md-8 " style="padding: 50px 60px">
+          <h2 style="margin-bottom:60px">Our Values</h2>
+          <h4 style="margin-bottom:40px"><strong>MISSION:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptate
+            blanditiis aliquam dolor, eum veritatis odit non error earum eius nesciunt nostrum saepe culpa, ducimus
+            quod, facere doloremque necessitatibus voluptatum!</h4>
+          <p><strong>VISION:</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illum qui explicabo
+            aut ea consequatur eius nam doloribus magnam, magni pariatur quam quaerat, soluta assumenda minus nihil
+            saepe optio fugit. Eius, dolorum iusto minima ipsum nobis explicabo dolor deleniti harum.</p>
+          <button class="btn btn-outline-secondary btn-lg">Get in Touch</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-sm-12 col-lg-8" ></div>
+  </div>
+
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
