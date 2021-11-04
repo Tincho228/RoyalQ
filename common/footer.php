60c2b0207ce795fcd254ee39e20008f29bb47d04
Tincho228 <martin_quintero521@hotmail.com>
2021-11-04 10:21:28 -0300

    test

--- common/footer.php
+++ common/footer.php
@@ -2,46 +2,44 @@
   <div class="container-full bg-dark" style="padding-top:30px ;
               padding-bottom:50px;       
        ">
-    
+
     <hr class=" bg-light">
-          <img src="./images/logo_purple.png" alt="logo of royalq" style="
+    <img src="./images/logo_purple.png" alt="logo of royalq" style="
                                                                     width:100px;
                                                                     display:block;
                                                                     margin:10px auto;  ">
-          <p class="text-light text-center">ROYALQ-MUNDIAL 2021 | Todos los derechos reservados</p>
-    </div>
-    <div class="accordion fixed-bottom" id="accordionExample">
-      <div class="card" style="border:transparent">
-        <div class="card-header d-flex bg-highlight justify-content-center" id="headingOne"
-          style="padding:0px; border:transparent; ; border-radius:0px">
-          <h2 class="mb-0">
-            <button  class="btn-footer text-center" type="button" data-toggle="collapse" data-target="#collapseOne"
-              aria-expanded="true" aria-controls="collapseOne" style=" margin-bottom:-7px;">
-              <div class="bg-third-color" style="border-radius: 30px 30px 0px 0px; padding-bottom:2px">
-                <i class="fas fa-chevron-up text-warning blink" style="font-size: 30px;padding:2px 10px"></i>
-              </div>
-            </button>
-          </h2>
-        </div>
+    <p class="text-light text-center">ROYALQ-MUNDIAL 2021 | Todos los derechos reservados</p>
+  </div>
 
-        <div id="collapseOne" class="collapse bg-third-color" aria-labelledby="headingOne"
-          data-parent="#accordionExample">
-          <div class="card-body text-light">
-            <p class="text-center text-light">Seguinos en redes sociales</p>
-            <hr class="bg-light">
-            <div class="d-flex justify-content-center">
-              <a href="https://api.whatsapp.com/send?phone=542604375615" target="_blank">
-                <i class="fab fa-whatsapp text-light" style="font-size: 40px; margin:0px 5px;"></i>
-              </a>
-              <a href="https://api.whatsapp.com/send?phone=542604375615" target="_blank">
-                <i class="fab fa-facebook text-light" style="font-size: 40px;margin:0px 5px;"></i>
-              </a>
-              <a href="https://api.whatsapp.com/send?phone=542604375615" target="_blank">
-                <i class="fab fa-telegram text-light" style="font-size: 40px;margin:0px 5px;""></i>
-      </a>
-    </div>
+  <div class="accordion fixed-bottom" id="accordionExample">
+    <div class="card " style="border:transparent; visibility:hidden ">
+      <div class="card-header d-flex  justify-content-center " id="headingOne"
+        style="padding:0px; border:transparent; ; border-radius:0px;">
+        <button class="btn-footer text-center" type="button" data-toggle="collapse" data-target="#collapseOne"
+          aria-expanded="true" aria-controls="collapseOne" style=" margin-bottom:-7px;visibility: visible">
+          <div class="bg-third-color" style="border-radius: 30px 30px 0px 0px; padding-bottom:2px">
+            <i class="fas fa-chevron-up text-warning blink" style="font-size: 30px;padding:2px 10px"></i>
+          </div>
+        </button>
+      </div>
+      <div id="collapseOne" class="collapse bg-third-color" aria-labelledby="headingOne"
+        data-parent="#accordionExample" style="visibility: visible">
+        <div class="card-body text-light">
+          <p class="text-center text-light">Seguinos en redes sociales</p>
+          <hr class="bg-light">
+          <div class="d-flex justify-content-center">
+            <a href="https://api.whatsapp.com/send?phone=542604375615" target="_blank">
+              <i class="fab fa-whatsapp text-light" style="font-size: 40px; margin:0px 5px;"></i>
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=542604375615" target="_blank">
+              <i class="fab fa-facebook text-light" style="font-size: 40px;margin:0px 5px;"></i>
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=542604375615" target="_blank">
+              <i class="fab fa-telegram text-light" style="font-size: 40px;margin:0px 5px;""></i>
+            </a>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </footer>
