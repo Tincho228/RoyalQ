4e9d688354d171c80134b226b62ad37696c48601
Tincho228 <martin_quintero521@hotmail.com>
2021-11-08 15:49:21 -0300

    testing

--- views/modal-contact.php
+++ views/modal-contact.php
@@ -5,7 +5,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content bg-highlight">
             <div class="modal-header transparent align-items-center">
-                <img class="mail_icon" src="./Images/logo.png" alt="email icon" style="width:60px;"> 
+                <img class="mail_icon" src="./images/logo.png" alt="email icon" style="width:60px;"> 
                 <h5 class="modal-title text-white" style="margin-left:10px;" id="exampleModalLabel">Envianos tu
                     pregunta.</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
